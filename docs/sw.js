d53ab22dc52362f65a6f97b542acd827a964281e
Bouwe K. Westerdijk <bouwe77@users.noreply.github.com>
2021-10-03 20:06:57 +0000

    Publish Markdown to HTML

---
+++ docs/sw.js
@@ -0,0 +1,16 @@
+// This script "kills" Gatsby: https://noelwelsh.com/posts/2019-07-11-gatsby-is-a-virus.html
+
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll();
+    })
+    .then(function (clients) {
+      clients.forEach((client) => client.navigate(client.url));
+    });
+});

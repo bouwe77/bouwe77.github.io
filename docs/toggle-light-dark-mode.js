50e5df64262112e7096d4c327f31f634163f6af7
Bouwe K. Westerdijk <bouwe77@users.noreply.github.com>
2023-04-08 22:24:01 +0000

    Publish Markdown to HTML

--- docs/toggle-light-dark-mode.js
+++ docs/toggle-light-dark-mode.js
@@ -1,12 +1,40 @@
+const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
+
+// Listen to color scheme changes in the system settings
+mediaQueryList.addEventListener('change', (event) => {
+  const isDark = event.matches
+  setTheme(isDark)
+  setToggleIcon(isDark)
+})
+
+// Initialize the theme when opening the page
+function initLightDarkMode() {
+  const isDark = mediaQueryList.matches
+  setTheme(isDark)
+  setToggleIcon(isDark)
+}
+
+// Toggle the light and dark themes when clicking the toggle button
 function toggleLightDarkMode() {
-  // Toggle the sun and moon icons
+  const isDark = document.body.classList.contains('dark')
+  setTheme(!isDark)
+  setToggleIcon(!isDark)
+}
+
+function setToggleIcon(isDark) {
   const svg = document.getElementById('sun-moon')
-  if (svg.getAttribute('viewBox') === '0 0 24 24') {
+  if (isDark) {
     svg.setAttribute('viewBox', '24 0 24 24')
   } else {
     svg.setAttribute('viewBox', '0 0 24 24')
   }
+}
 
-  // Toggle the light and dark themes
-  document.body.classList.toggle('dark')
+// Apply the theme
+function setTheme(isDark) {
+  if (isDark) {
+    document.body.classList.add('dark')
+  } else {
+    document.body.classList.remove('dark')
+  }
 }

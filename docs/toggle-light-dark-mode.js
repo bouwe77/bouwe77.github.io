276c31f4ef772eff3c05b0ea309b7694fe21943f
Bouwe K. Westerdijk <bouwe77@users.noreply.github.com>
2023-05-14 21:00:36 +0000

    Publish Markdown to HTML

--- docs/toggle-light-dark-mode.js
+++ docs/toggle-light-dark-mode.js
@@ -3,27 +3,37 @@ const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
 // Listen to color scheme changes in the system settings
 mediaQueryList.addEventListener('change', (event) => {
   const isDark = event.matches
-  setTheme(isDark)
-  setToggleIcon(isDark)
+  setTheme(isDark ? 'dark' : 'light')
+  setToggleIcon(isDark ? 'sun' : 'moon')
 })
 
 // Initialize the theme when opening the page
 function initLightDarkMode() {
-  const isDark = mediaQueryList.matches
-  setTheme(isDark)
-  setToggleIcon(isDark)
+  let isDark = true
+
+  // Check if a preference is set in localStorage.
+  // If not, check if the system is set to dark mode.
+  const theme = localStorage.getItem('theme')
+  if (theme === 'light') isDark = false
+  else isDark = mediaQueryList.matches
+
+  setTheme(isDark ? 'dark' : 'light')
+  setToggleIcon(isDark ? 'sun' : 'moon')
 }
 
 // Toggle the light and dark themes when clicking the toggle button
 function toggleLightDarkMode() {
-  const isDark = document.body.classList.contains('dark')
-  setTheme(!isDark)
-  setToggleIcon(!isDark)
+  const willBecomeLight = document.body.classList.contains('dark')
+
+  localStorage.setItem('theme', willBecomeLight ? 'light' : 'dark')
+
+  setTheme(willBecomeLight ? 'light' : 'dark')
+  setToggleIcon(willBecomeLight ? 'moon' : 'sun')
 }
 
-function setToggleIcon(isDark) {
+function setToggleIcon(icon) {
   const svg = document.getElementById('sun-moon')
-  if (isDark) {
+  if (icon === 'sun') {
     svg.setAttribute('viewBox', '24 0 24 24')
   } else {
     svg.setAttribute('viewBox', '0 0 24 24')
@@ -31,8 +41,8 @@ function setToggleIcon(isDark) {
 }
 
 // Apply the theme
-function setTheme(isDark) {
-  if (isDark) {
+function setTheme(theme) {
+  if (theme === 'dark') {
     document.body.classList.add('dark')
   } else {
     document.body.classList.remove('dark')

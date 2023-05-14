8699dfb116de234f9208a5203907a6ac8f073a5f
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2023-05-14 22:41:56 +0200

    refactor: explicit state instead of booleans

--- content/static/toggle-light-dark-mode.js
+++ content/static/toggle-light-dark-mode.js
@@ -3,27 +3,27 @@ const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
 // Listen to color scheme changes in the system settings
 mediaQueryList.addEventListener('change', (event) => {
   const isDark = event.matches
-  setTheme(isDark)
-  setToggleIcon(isDark)
+  setTheme(isDark ? 'dark' : 'light')
+  setToggleIcon(isDark ? 'sun' : 'moon')
 })
 
 // Initialize the theme when opening the page
 function initLightDarkMode() {
   const isDark = mediaQueryList.matches
-  setTheme(isDark)
-  setToggleIcon(isDark)
+  setTheme(isDark ? 'dark' : 'light')
+  setToggleIcon(isDark ? 'sun' : 'moon')
 }
 
 // Toggle the light and dark themes when clicking the toggle button
 function toggleLightDarkMode() {
   const isDark = document.body.classList.contains('dark')
-  setTheme(!isDark)
-  setToggleIcon(!isDark)
+  setTheme(isDark ? 'light' : 'dark')
+  setToggleIcon(isDark ? 'moon' : 'sun')
 }
 
-function setToggleIcon(isDark) {
+function setToggleIcon(icon) {
   const svg = document.getElementById('sun-moon')
-  if (isDark) {
+  if (icon === 'sun') {
     svg.setAttribute('viewBox', '24 0 24 24')
   } else {
     svg.setAttribute('viewBox', '0 0 24 24')
@@ -31,8 +31,8 @@ function setToggleIcon(isDark) {
 }
 
 // Apply the theme
-function setTheme(isDark) {
-  if (isDark) {
+function setTheme(theme) {
+  if (theme === 'dark') {
     document.body.classList.add('dark')
   } else {
     document.body.classList.remove('dark')
